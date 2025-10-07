af93ca0e048441f967d17919649e1eade1008b30
aicrus <93139175+Aicrus@users.noreply.github.com>
2025-10-07 16:16:07 -0300

    Atualiza dependências do projeto Flutter, incluindo a adição do pacote supabase_flutter e a configuração de inicialização do Supabase no arquivo main.dart. Também foram feitas alterações nas configurações do iOS e macOS para incluir suporte a plugins, além de registrar plugins adicionais no Linux e Windows.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://fovtdamjplseciadcvtt.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvdnRkYW1qcGxzZWNpYWRjdnR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4NjEzNDUsImV4cCI6MjA3NTQzNzM0NX0.3BHgkefw6BnHfaM8K3crkdiP4B7Otyu3X9yv9HWqEJI',
+  );
   runApp(const MyApp());
 }
 
